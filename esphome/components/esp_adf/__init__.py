b1ba27487d6b12c0f3967f97d0631da80ce5820c
xamakadesigns <12724275+xamakadesigns@users.noreply.github.com>
2025-02-01 17:12:31 -0600

    esp32korvo1

--- esphome/components/esp_adf/__init__.py
+++ esphome/components/esp_adf/__init__.py
@@ -23,6 +23,7 @@ SUPPORTED_BOARDS = {
     "esp32s3boxlite": "CONFIG_ESP32_S3_BOX_LITE_BOARD",
     "esp32s3box3": "CONFIG_ESP32_S3_BOX_3_BOARD",
     "esp32s3korvo1": "CONFIG_ESP32_S3_KORVO1_BOARD",
+    "esp32korvo1": "CONFIG_ESP32_S3_KORVO1_BOARD",
 }
 
 
